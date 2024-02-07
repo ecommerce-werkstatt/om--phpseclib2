3d3d5dfe80b3ea7301f027d2e0994a8f1bd1b978
AGelzer <gelzeralexander@gmail.com>
2024-02-07 11:19:29 +0100

    add phpseclib2 2.0.46

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93e8036c2fce35cd3b9d9e620fb064d9fc71fff8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93e8036c2fce35cd3b9d9e620fb064d9fc71fff8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpseclib/phpseclib' => array(
+            'pretty_version' => '2.0.46',
+            'version' => '2.0.46.0',
+            'reference' => '498e67a0c82bd5791fda9b0dd0f4ec8e8aebb02d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpseclib/phpseclib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
